95455a300d6f11ffa675e6199f1f74f750259be3
Jazmin Aguilar <rjazminaguilar@gmail.com>
2020-04-19 18:27:05 -0500

    Style messagePreview

--- src/components/List.js
+++ src/components/List.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import MessagePreview from "./MessagePreview";
 
 export default class List extends Component {
+
     render() {
+        const {seconds} = this.state;
         return (
             <div className="lista">
-                <MessagePreview/>
+                {seconds}
+                <MessagePreview unread={true}/>
+                <MessagePreview unread={false}/>
             </div>
         )
     }
--- src/components/MessagePreview.js
+++ src/components/MessagePreview.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 
 export default class MessagePreview extends Component {
+
     render() {
+        const {unread} = this.props;
+
         return (
-            <div className="messagePreview">
+            <div className={`messagePreview ${unread ? "read": ""}`}>
                 <div className="large_block">
-                    <div className="sender">Croswell Bowen</div>
+                    <div className="sender"><b>Croswell Bowen</b></div>
                     <span className="preview">Lorem Ipsum is simply dummy</span>
                 </div>
                 <div className="small_block">
                     <div className="hour">09:15 AM</div>
                     <div className="attachment"><img className="icon" alt="clip" src="https://img.icons8.com/ios/50/000000/attach.png"/></div>
                 </div>
-
             </div>
         )
     }
