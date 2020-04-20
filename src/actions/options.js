8315499ddd3b225e7836cbaaf6565b43c6836379
Jazmin Aguilar <rjazminaguilar@gmail.com>
2020-04-19 21:52:50 -0500

    Search

---
+++ src/actions/options.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const IS_SPAM= 'IS SPAM'
+
+export function isSpam{
+    return (dispatch, getState){
+        (axios.get'json')
+    }
+}
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,8 @@ export default class Search extends Component {
     render() {
         return (
             <div className="search">
-                  <input type="text" placeholder="Search.."></input>
+                <input type="text" placeholder="Search.."></input>
+                <span class="icon"><i class="fa fa-search"></i></span>
             </div>
         )
     }
